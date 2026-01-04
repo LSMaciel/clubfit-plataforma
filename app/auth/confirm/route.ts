6e290fe5a5d49b691cc297f90ddfae79aed3d143
LSMaciel <lucasmaciel.hc@gmail.com>
2026-01-04 12:41:24 -0300

    Correcao typo const token hash

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -4,7 +4,7 @@ import { createClient } from '@/utils/supabase/server'
 
 export async function GET(request: NextRequest) {
     const { searchParams } = new URL(request.url)
-    consttoken_hash = searchParams.get('token_hash')
+    const token_hash = searchParams.get('token_hash')
     const type = searchParams.get('type') as EmailOtpType | null
     const next = searchParams.get('next') ?? '/admin/dashboard'
 
